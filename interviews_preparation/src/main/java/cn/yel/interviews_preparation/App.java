bab071a9c4f9e148b526f65ba6634e006cbbc0fe
Bravico <bravicohc@gmail.com>
2018-06-24 15:48:48 +0800

    first commit for git

---
+++ interviews_preparation/src/main/java/cn/yel/interviews_preparation/App.java
@@ -0,0 +1,13 @@
+package cn.yel.interviews_preparation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
